aae02699ec3c1528c9542675cb14e8327db56f9b
Kriszchan <panyik.krisztian@students.jedlik.eu>
2024-01-15 17:49:38 +0100

    valami

--- flexgo.js
+++ flexgo.js
@@ -36,3 +36,45 @@ function GameRender() {
 }
 
 window.addEventListener("resize", GameRender);
+function genMap(){
+    let mapContainer = document.getElementById("map")
+    let curX = 500
+    let curY = 0
+    fetch('testlevel.txt')
+    .then((res) => res.text())
+    .then((text) => {
+        let lines = text.split("\n")
+        for (let y = 0; y < 18; y++) {
+            for (let x = 0; x < 36; x++) {
+                let Nums = lines[y].split(" ")
+                let curNum = parseInt(Nums[x])
+                let tile = document.createElement("div")
+                let coords = [curX, curY]
+                console.log(typeof(curNum))
+                if(curNum == 0){
+                    tile.className = "sima"
+                }
+                else if(curNum == 1){
+                    tile.className = "fal"
+                    this.colliders.push(coords)
+                }
+                else if(curNum == 2){
+                    tile.className = "coin"
+                    this.colliders.push(coords)
+                    this.winCol.push(coords)
+                }
+                else if(curNum == 3){
+                    tile.className = "restart"
+                    this.killCols.push(coords)
+                    this.colliders.push(coords)
+                }
+                tile.style.top = curY + "px"
+                tile.style.left = curX + "px"
+                mapContainer.append(tile)
+                curX += 50
+            }
+            curX = 500
+            curY += 50
+        }
+    })
+}
