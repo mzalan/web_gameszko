ca237fd5895013e0b533a9616bc885f01e79db87
mzalan <marquetant.zalan@students.jedlik.eu>
2024-01-15 08:35:03 +0100

    játék grid

--- flexgo.js
+++ flexgo.js
@@ -3,20 +3,36 @@ function GameRender() {
     let width = window.innerWidth;
     let jatekHeight;
     let jatekWidth;
+    let jatekLeft;
+    let jatekTop;
     if(height * 1.8 > width) {
         jatekWidth = width;
         jatekHeight = width / 1.8;
-        jatekterulet.style.top = (height - jatekHeight) / 2 + "px";
-        jatekterulet.style.left = "0px";
+        jatekTop = (height - jatekHeight) / 2;
+        jatekLeft = 0;
     }
     else {
         jatekWidth = height * 1.8;
         jatekHeight = height;
-        jatekterulet.style.top = "0px";
-        jatekterulet.style.left = (width - jatekWidth) / 2 +"px";
+        jatekTop = 0;
+        jatekLeft = (width - jatekWidth) / 2;
     }
     jatekterulet.style.width = jatekWidth+"px";
     jatekterulet.style.height = jatekHeight+"px";
+    jatekterulet.style.top = jatekTop + "px";
+    jatekterulet.style.left = jatekLeft + "px";
+    let tileSize = jatekWidth / 36;
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 36; j++)  {
+            let tile = document.createElement("span");
+            tile.className = "tile";
+            tile.style.width = tileSize+"px";
+            tile.style.height = tileSize+"px";
+            tile.style.left = j * tileSize +  jatekLeft + "px";
+            tile.style.top = i * tileSize + jatekTop + "px";    
+            jatekterulet.appendChild(tile);
+        } 
+    }
 }
 
 window.addEventListener("resize", GameRender);
