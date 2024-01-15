4a37060e00055f16979444c4e71d0d602e60f7de
mzalan <marquetant.zalan@students.jedlik.eu>
2024-01-15 06:31:44 +0100

    dinamikus méretű játékterület

--- flexgo.js
+++ flexgo.js
@@ -1,8 +1,22 @@
-const height = window.innerHeight;
-const width = window.innerWidth;
-
-if(height > width) {
-    
+function GameRender() { 
+    let height = window.innerHeight;
+    let width = window.innerWidth;
+    let jatekHeight;
+    let jatekWidth;
+    if(height * 1.8 > width) {
+        jatekWidth = width;
+        jatekHeight = width / 1.8;
+        jatekterulet.style.top = (height - jatekHeight) / 2 + "px";
+        jatekterulet.style.left = "0px";
+    }
+    else {
+        jatekWidth = height * 1.8;
+        jatekHeight = height;
+        jatekterulet.style.top = "0px";
+        jatekterulet.style.left = (width - jatekWidth) / 2 +"px";
+    }
+    jatekterulet.style.width = jatekWidth+"px";
+    jatekterulet.style.height = jatekHeight+"px";
 }
-alert(height);
-alert(width);
+
+window.addEventListener("resize", GameRender);
